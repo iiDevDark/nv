9ae0abbcfbd9ae5443140914949e1afc0bc2fad3
iiDevDark <107908903+iiDevDark@users.noreply.github.com>
2022-11-16 02:34:53 +0200

    Update Footer.jsx

--- components/Static/Footer.jsx
+++ components/Static/Footer.jsx
@@ -54,7 +54,7 @@ export default function Footer() {
                                     Invite Bot
                                 </a>
                             </div>
-  
+        </div>
                         <div className="col-span-1">
                             <p className="text-white font-medium mt-3 sm:mt-0 sm:mb-3">Important</p>
                             <div>
